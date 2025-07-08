436e5b9b810087db822fe64a376ea72fc825b454
Santiago Cardona Herrera <santiago.cardona-herrera@flyr.com>
2025-07-08 07:19:39 -0500

    arreglo 5

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const equipos = [
-  { nombre: "Custom 3 - Core I y II", integrantes: 13, asistencia: "On-site" },
-  { nombre: "Custom 5 - Dynamics", integrantes: 10, asistencia: "On-site" },
-  { nombre: "Producto", integrantes: 6, asistencia: "On-site" },
-  { nombre: "Consilium", integrantes: 2, asistencia: "On-site" },
-  { nombre: "Automatizaciones", integrantes: 3, asistencia: "On-site" },
-  { nombre: "Vueling", integrantes: 11, asistencia: "On-site" },
-  { nombre: "Riyadh ", integrantes: 15, asistencia: "On-site" },
-  { nombre: "Cyprus - Message - Infras - Infra Members", integrantes: 5, asistencia: "On-site" },
-  { nombre: "Sun Express", integrantes: 7, asistencia: "On-site" },
-  { nombre: "Next Gen (Delta - Omega - AM ) y Core UI", integrantes: 17, asistencia: "On-site" },
-  { nombre: "Custom 1 - Platform ", integrantes: 7, asistencia: "On-site" },
+  { nombre: "Custom 3 - Core I y II", integrantes: 13, asistencia: "On-site", DiasDeasistencia: "Lunes, Jueves, Viernes" },
+  { nombre: "Custom 5 - Dynamics", integrantes: 10, asistencia: "On-site", DiasDeasistencia: "Martes, Miércoles, Jueves" },
+  { nombre: "Producto", integrantes: 6, asistencia: "On-site" , DiasDeasistencia: "Martes, Miércoles" },
+  { nombre: "Consilium", integrantes: 2, asistencia: "On-site", DiasDeasistencia: "Jueves, Viernes" },
+  { nombre: "Automatizaciones", integrantes: 3, asistencia: "On-site", DiasDeasistencia: "Miércoles, Jueves" },
+  { nombre: "Vueling", integrantes: 11, asistencia: "On-site", DiasDeasistencia: "Lunes, Jueves" },
+  { nombre: "Riyadh ", integrantes: 15, asistencia: "On-site" , DiasDeasistencia: "Martes, Jueves, Viernes" },
+  { nombre: "Cyprus - Message - Infras - Infra Members", integrantes: 5, asistencia: "On-site" , DiasDeasistencia: "Miércoles, Jueves, Viernes" },
+  { nombre: "Sun Express", integrantes: 7, asistencia: "On-site" , DiasDeasistencia: "Martes, Miercoles, Viernes" },
+  { nombre: "Next Gen (Delta - Omega - AM ) y Core UI", integrantes: 17, asistencia: "On-site" , DiasDeasistencia: "Lunes, Martes, Miercoles" },
+  { nombre: "Custom 1 - Platform ", integrantes: 7, asistencia: "On-site" , DiasDeasistencia: "Miercoles, Jueves, Viernes" },
 ];
 
 const resultado = document.getElementById("resultado");
@@ -27,6 +27,7 @@ function mostrarEquipos(lista) {
         <h3>🧩 Equipo: ${eq.nombre}</h3>
         <p><strong>Integrantes:</strong> ${eq.integrantes}</p>
         <p><strong>Tipo de Asistencia:</strong> ${eq.asistencia}</p>
+         <p><strong>Dias de Asistencia:</strong> ${eq.DiasDeasistencia}</p>
       </div>
     `)
     .join("");
